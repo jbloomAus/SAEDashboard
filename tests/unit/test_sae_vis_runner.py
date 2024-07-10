4725ffa2cbe743aa0bb615213f11105b6911f10d
Joseph Bloom <jbloomaus@gmail.com>
2024-07-10 14:14:05 +0000

    linter

--- tests/unit/test_sae_vis_runner.py
+++ tests/unit/test_sae_vis_runner.py
@@ -12,7 +12,6 @@ from sae_dashboard.components_config import SequencesConfig
 from sae_dashboard.data_writing_fns import save_feature_centric_vis
 from sae_dashboard.sae_vis_data import SaeVisConfig, SaeVisData
 from sae_dashboard.sae_vis_runner import SaeVisRunner
-from tests.helpers import round_floats_deep
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 N_FEATURES = 32
